97351fdd145fa9ae13c39c46456ec20fe718ce25
peterlebad <saleh@etud.insa-toulouse.fr>
2013-12-16 10:47:22 +0100

    clean repo

--- db/migrate/20131121205717_add_index_to_users_email.rb
+++
@@ -1,7 +0,0 @@
-class AddIndexToUsersEmail < ActiveRecord::Migration
-  def change
-  	#On ajoute un index sur la colonne email de la table users
-  	#On s'assure de son unicité 
-  	add_index :users, :email, unique: true
-  end
-end
